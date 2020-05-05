4d0570dcec23d896a4c4673298d81fd7046e190d
Matthew Lewis <malewis5@crimson.ua.edu>
2020-05-05 11:15:28 -0400

    redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider
+    store={createStore(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
